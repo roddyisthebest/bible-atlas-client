0a06fa1990767444611298e56a9558542efb764c
shy <shy@lemoncloud.io>
2025-02-03 13:54:29 +0900

    chore: initialize Xcode project

---
+++ BibleAtlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BibleAtlas
+//
+//  Created by 배성연 on 2/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
