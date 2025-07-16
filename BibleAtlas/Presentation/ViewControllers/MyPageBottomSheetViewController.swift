94e61f2c2bcca079da8c6b6d67f86ecf855e2b84
shy-lemon <shy@lemoncloud.io>
2025-07-16 22:31:11 +0900

    feat(homeBottomVC): show user avatar image after login

---
+++ BibleAtlas/Presentation/ViewControllers/MyPageBottomSheetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPageBottomSheetViewController.swift
+//  BibleAtlas
+//
+//  Created by 배성연 on 7/16/25.
+//
+
+import UIKit
+
+class MyPageBottomSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
